023b54d80c5c40cad3a5a61db8936923f03c6590
iva-smth <108048159+iva-smth@users.noreply.github.com>
2025-03-20 22:04:36 +0700

    Update urls.py

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,13 +1,20 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import TemplateView
+from django.contrib import admin  # Импортируем модуль админки Django
+from django.urls import include, path  # Импортируем функции для работы с URL-маршрутами
+from django.views.generic import TemplateView  # Импортируем класс для отображения статических страниц
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    #Доступ к интерфейсу администратора.
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns.append(
+    # Маршрут для документации API (Redoc).
+    # При переходе на `/redoc/` будет отображаться статическая HTML-страница с документацией.
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
+        name='redoc'  
     ),
-]
+)
